736d3c643d761a100e036cd5f2621aef9fcbf0fb
zhentoufei <zhentoufei@163.com>
2017-06-22 23:08:40 +0800

    取出首页文章的视图

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,3 +1,16 @@
+# coding:utf-8
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import Article
 # Create your views here.
+
+
+class ArticleView(View):
+    '''
+    获取主页的文章
+    '''
+    def get(self, request):
+        all_article = Article.objects.all()
+        return render(request, 'index.html', {
+            'all_article': all_article
+        })
