b0e707888ef22258151ac4610f571a393edc73e4
zhentoufei <zhentoufei@163.com>
2017-06-26 23:21:47 +0800

    文章的content设置成了ueditro

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from DjangoUeditor.models import UEditorField
 # Create your models here.
 
 class Tag(models.Model):
@@ -20,8 +20,9 @@ class Article(models.Model):
     category = models.CharField(max_length = 50, blank = True)  #博客分类 可为空
     tag = models.ForeignKey(Tag, blank=True, default='')  # 博客标签 可为空
     date_time = models.DateTimeField(auto_now_add = True)  #博客日期
-    content = models.TextField(blank = True, null = True)  #博客文章正文
-
+    # content = models.TextField(blank = True, null = True)  #博客文章正文
+    content = UEditorField(verbose_name=u'博客内容', width=900, height=500, imagePath="courses/ueditor/",
+                          filePath="courses/ueditor/",blank=True)
 
 
     class Meta:
