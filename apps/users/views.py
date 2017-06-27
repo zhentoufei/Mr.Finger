c550a6550c4f4f56e462e17188c65929e8642b11
zhentoufei <zhentoufei@163.com>
2017-06-27 23:11:29 +0800

    增加错误页面的视图处理函数

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def page_not_found(request):
+    from django.shortcuts import render_to_response
+    response = render_to_response('error404.html',{})
+    response.status_code=404
+    return response
