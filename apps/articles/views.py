ea3dd04a3fc51cf6f3359722b27b9e2d9ac9e94d
zhentoufei <zhentoufei@163.com>
2017-06-27 16:59:07 +0800

    增加文章详情页面的视图函数&首页文章显示控制在5篇

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -10,7 +10,20 @@ class ArticleView(View):
     获取主页的文章
     '''
     def get(self, request):
-        all_article = Article.objects.all()
+        all_article = Article.objects.all()[:5]
         return render(request, 'index.html', {
             'all_article': all_article
+        })
+
+
+class ArticleDetailView(View):
+    '''
+    获取文章的详情
+    '''
+    def get(self, request, article_id):
+        article = Article.objects.get(id=int(article_id))
+        flag = 'detail'
+        return render(request, 'detail.html', {
+            "article":article,
+            "flag":flag
         })
