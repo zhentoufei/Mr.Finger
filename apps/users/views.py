a44e55588db05f0906f77931b710c50a8647c294
zhentoufei <zhentoufei@163.com>
2017-06-29 19:22:13 +0800

    增加“关于”页面的视图

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,7 @@
+# coding:utf-8
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import UserAbout
 # Create your views here.
 
 
@@ -8,3 +10,16 @@ def page_not_found(request):
     response = render_to_response('error404.html',{})
     response.status_code=404
     return response
+
+
+class UserAboutView(View):
+    '''
+    取出关于用户信息
+    '''
+    def get(self, request):
+        about = UserAbout.objects.all()[0]
+        flag = 'about'
+        return render(request, 'about.html',{
+            "about":about,
+            'flag':flag
+        })
