130df679195a54119032dd385b9d5f03f947fe1d
zhentoufei <zhentoufei@163.com>
2017-06-29 19:21:47 +0800

    增加“关于”页面的模型

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,6 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from DjangoUeditor.models import UEditorField
+from datetime import datetime
+
+
 # Create your models here.
 
 
@@ -13,9 +18,24 @@ class UserProfile(AbstractUser):
     address = models.CharField(max_length=100, default=u"")
     mobile = models.CharField(max_length=11, null=True, blank=True)
     detail = models.TextField(max_length=1000, blank=True)
+
     class Meta:
         verbose_name = "用户信息"
         verbose_name_plural = verbose_name
 
     def __unicode__(self):
         return self.username
+
+
+class UserAbout(models.Model):
+    about = UEditorField(verbose_name=u'博客==内容', width=900, height=500, imagePath="ueditor/%Y/%m",
+                         filePath="ueditor/%Y/%m", blank=True, default="")
+
+    date_time = models.DateTimeField(verbose_name=u'更新日期', default=datetime.now)  # 更改日期
+
+    class Meta:
+        verbose_name = u"关于"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return u'关于我'
