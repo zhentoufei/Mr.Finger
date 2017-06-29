3db0bf7a271e3b7cc76804e869bfc45a1b35bdcb
zhentoufei <zhentoufei@163.com>
2017-06-29 19:18:21 +0800

    adminx用户about的注册

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -5,3 +5,14 @@ import xadmin
 #     pass
 #
 # xadmin.site.register(UserProfile, UserProfileAdmin)
+from .models import UserAbout
+import xadmin
+
+
+class AboutAdmin(object):
+    list_display = ['about', 'date_time']
+    style_fields = {"about":"ueditor"}
+
+
+
+xadmin.site.register(UserAbout, AboutAdmin)
